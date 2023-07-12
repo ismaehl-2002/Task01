4d7a5461901df5ea198f523f6236de54a3372b28
Ismail Chaudhary <ismailchaudhary2002@gmail.com>
2023-07-12 15:48:37 +0530

    Calculator Done, GuessNumber

---
+++ Task/Calculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Task01
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
+
+
+                Console.WriteLine("Calculator Program");
+
+                Console.Write("Enter number: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.WriteLine("Enter a Operation");
+                Console.WriteLine("\t + : Addition");
+                Console.WriteLine("\t - : Substraction");
+                Console.WriteLine("\t * : Multiplication");
+                Console.WriteLine("\t / : Division");
+
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                        break;
+
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                        break;
+
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                        break;
+
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                        break;
+                    default:
+                        Console.WriteLine("That was not a valid option");
+                        break;
+                }
+                Console.Write("Would you like to continue? (Y = yes, N = No): ");
+            }
+
+            while (Console.ReadLine().ToUpper() == "Y");
+            Console.WriteLine("Bye!");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Task/GuessNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task
+{
+    class GuessNumber
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Guess the Lucky Number \nCondition: Between 1 to 10");
+            Console.WriteLine("Let's Try Enter a Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 7)
+            {
+                Console.WriteLine("Correct! Have Good Day!");
+            }
+            else if (num > 7)
+            {
+                Console.WriteLine("Too Far");
+            }
+            else if (num < 7)
+            {
+                Console.WriteLine("Too Close");
+            }
+            else
+            {
+                Console.WriteLine("Better Chance See You Later!");
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
