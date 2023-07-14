ce573c2f0a4fc299bf47f5104a996cd007cb3f47
Ismail Chaudhary <ismailchaudhary2002@gmail.com>
2023-07-14 11:49:52 +0530

    Rock Paper Scissors is Done!

--- Task/Calculator.cs
+++ Task/Calculator.cs
@@ -61,5 +61,4 @@ namespace Task01
             Console.ReadKey();
         }
     }
-
 }
--- Task/GuessNumber.cs
+++ Task/GuessNumber.cs
@@ -2,7 +2,7 @@
 
 namespace Task
 {
-    class GuessNumber
+class GuessNumber
     {
         static void Main(string[] args)
         {
@@ -14,13 +14,37 @@ namespace Task
             {
                 Console.WriteLine("Correct! Have Good Day!");
             }
-            else if (num > 7)
+            else if (num == 6)
             {
-                Console.WriteLine("Too Far");
+                Console.WriteLine("Too Close Little Bit");
             }
-            else if (num < 7)
+            else if (num ==  5)
             {
-                Console.WriteLine("Too Close");
+                Console.WriteLine("Little Bit Close");
+            }
+            else if (num ==  4)
+            {
+                Console.WriteLine("Little Bit Close");
+            }
+            else if (num ==  3)
+            {
+                Console.WriteLine("You r Left behind the 3 numbers");
+            }
+            else if (num ==  2)
+            {
+                Console.WriteLine("You are going too Reverse");
+            }
+            else if (num ==  8)
+            {
+                Console.WriteLine("Little Far");
+            }
+            else if (num ==  9)
+            {
+                Console.WriteLine("Far try next");
+            }
+            else if (num ==  10)
+            {
+                Console.WriteLine("Nope");
             }
             else
             {
